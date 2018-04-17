5323fd3dfb17e3a5e26f4ba1dfad9226c8549320
Eric Noguchi <eric_noguchi@hotmail.com>
2018-04-16 20:46:46 -0400

    addi ng express data

--- _server/index.js
+++ _server/index.js
@@ -1,6 +1,36 @@
 const express = require('express')
 const app = express()
 
-app.get('/', (req, res) => res.send('Hello World!'))
+const users = [{
+        "id": 1,
+        "fullName": "Allison Finch",
+        "bool": true
+    },
+    {
+        "id": 2,
+        "fullName": "Jeanette Drake",
+        "bool": true
+    },
+    {
+        "id": 3,
+        "fullName": "Hilda Anderson",
+        "bool": false
+    },
+    {
+        "id": 4,
+        "fullName": "Sherri Boyette",
+        "bool": false
+    },
+    {
+        "id": 5,
+        "fullName": "Ashley Boyd",
+        "bool": true
+    }
+]
+
+
+
+
+app.get('/users', (req, res) => res.json(users))
 
 app.listen(3000, () => console.log('Example app listening on port 3000!'))
